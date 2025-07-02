3a8aa87cdac438cd43c4c273337c81a93c591722
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-07-02 21:58:10 +0700

    finsih : tutorial ts-react 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,68 @@
 
-import { useState } from 'react'
-import './App.css'
+import { useState, type ReactNode } from 'react'
 
 
 type User = {
-  email : string,
-  password : string
+  email: string,
+  password: string
 }
 
-function App() {
-  const [count , setCount] = useState<number>(0)
-  const [user ,setUser] = useState<User>()
+type ButtonProps = {
+  className?: string
+  onClick: () => void
+  children: ReactNode
+}
 
+const Button = (props: ButtonProps) => {
+  return (
+    <>
+      <button className={props.className} onClick={props.onClick}>
+        {props.children}
+      </button>
+    </>
+  )
+}
+
+
+
+function App() {
+  const [count, setCount] = useState<number>(0)
+  const [user, setUser] = useState<User>()
+  const [text,setText] = useState<number>(0)
 
   const handleCount = () => {
     setCount(count + 1)
   }
 
-  const handleUser =  () => {
-    setUser ({
-      email : "aldowiranata@gmail.com",
-      password : "12345678"
+  const handleUser = () => {
+    setUser({
+      email: "aldowiranata@gmail.com",
+      password: "12345678"
     })
   }
 
-  
-  return(
+  const handleText : React.ChangeEventHandler<HTMLInputElement>= (event) => {
+   const value = event.target.valueAsNumber
+   if(!isNaN(value)){
+    setText(value)
+   }else{
+    setText(0)
+   }
+  }
+
+  return (
     <>
-      <button onClick={handleCount} style={{margin : "10px"}}>Count + 1</button>
-      <button onClick={handleUser}  style={{margin : "10px"}}>Click</button>
+      <button onClick={handleCount} style={{ margin: "10px" }}>Count + 1</button>
+      <button onClick={handleUser} style={{ margin: "10px" }}>Click</button>
       <p>Count    : {count}</p>
       <p>Email    : {user?.email}</p>
       <p>Password : {user?.password}</p>
+
+      <input onChange={handleText} type='number' />
+      <div>{text}</div>
+      <Button onClick={() => { console.log("aldo") }} className="p">
+        Klik
+      </Button>
 
     </>
   )
