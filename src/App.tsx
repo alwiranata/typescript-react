deeeddaa1623e22bb3427c138ef5e2f9025aee37
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2025-07-09 21:19:35 +0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,51 @@
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import ProfileCard from "./components/ProfileCard"
+// import './App.css'
+
+type Students = {
+  id :string
+  name : string
+  nim : string
+  prodi : string
+}
+
+const students : Students[] = [
+  {
+    id : "1",
+    name : "Aldo",
+    nim : "2455201035",
+    prodi : "TI"
+  },
+  {
+    id : "2",
+    name : "Aldo",
+    nim : "2455201035",
+    prodi : "TI"
+  },
+  {
+    id : "3",
+    name : "Aldo",
+    nim : "2455201035",
+    prodi : "TI"
+  }
+  
+]
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <ProfileCard name="Aldo Wiranata" nim = "2455201036" prodi="Ti"/>
+    <ProfileCard name="Aldo Wiranata" nim = "2455201037" prodi="Ti"/>
+    <ProfileCard name="Aldo Wiranata" nim = "2455201038" prodi="Ilkom"/>
+
+    {students.map((student) => {
+      return <ProfileCard key={student.id}   name={student.name} nim={student.nim} prodi={student.prodi}/>
+    })}
+    <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,24 @@
+type ProfileCardProps = {
+    name : string,
+    nim : string
+    prodi ?: string
+}
+
+
+
+
+const ProfileCard = (props  :ProfileCardProps) => {
+    const {name = "Anonymus" , nim , prodi = false} = props
+
+    return (
+        <>
+        <div className="card">
+            <p>Name : {name}</p>
+            <p>NIM : {nim}</p>
+            {props && <p>Prodi:  {prodi} </p> }
+        </div>
+        </>
+    )
+}
+
+export default ProfileCard
